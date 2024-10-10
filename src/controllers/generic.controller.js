254f5980b7b26586843ca60708239f423983aa70
Alendroide <alendroideyt@gmail.com>
2024-10-10 09:53:33 -0500

    Controller extended

--- src/controllers/generic.controller.js
+++ src/controllers/generic.controller.js
@@ -1,19 +1,32 @@
 module.exports = {
     getAll : (Model,include=[]) => async (req,res) => {
         try {
-            const newModel = await Model.findAll({include});
-            return res.status(200).json(newModel);
+            const modelList = await Model.findAll({include});
+            return res.status(200).json(modelList);
         }
         catch(error){
             return res.status(500).json({message:`Error obteniendo ${Model.name}`})
         }
     },
-    getById : (Model,include=[]) => async (req,res) => {
+    getById : (Model,include=[],id='id') => async (req,res) => {
         try {
-            
+            const modelInstance = await Model.findByPk(req.params[id],{include});
+            if(!modelInstance){
+                return res.status(404).json({message:`${Model.name} no encontrado`})
+            }
+            return res.status(200).json(modelInstance);
         }
         catch(error){
-
+            return res.status(500).json({message:`Error obteniendo ${Model.name}`})
+        }
+    },
+    create : (Model) => async (req,res) => {
+        try{
+            const newModel = await Model.create(req.body);
+            return res.status(201).json(newModel);
+        }
+        catch(error){
+            return res.status(400).json({message:`Error creando ${Model.name}`})
         }
     }
 }
--- src/routers/estudiante.router.js
+++ src/routers/estudiante.router.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 const controller = require('../controllers/generic.controller');
 
 router.get('/estudiantes',controller.getAll(Estudiante))
-
+router.get('/estudiantes/:identificacion',controller.getById(Estudiante,[],'identificacion'));
+router.post('/estudiantes',controller.create(Estudiante));
 module.exports = router;
